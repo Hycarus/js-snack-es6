597ff7bbeb77ee29820ef2fd9a7e0fff9f2c7bf7
Hycarus <mirkomarasco08@gmail.com>
2023-10-24 13:42:20 +0200

    added first snack

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
+const resultEl = document.querySelector('.alert');
 
+const invitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 
-
+const segnaposto = [];
+invitati.forEach((element, index) => {
+    const invitato = {
+        guestName: element,
+        tableName: 'Tavolo Vip',
+        place: index + 1
+    }
+    segnaposto.push(invitato);
+});
+console.log(segnaposto);
 
 
 
