93a73fadc03daab909c4959a8040cf3e1ec4548f
Hycarus <mirkomarasco08@gmail.com>
2023-10-24 16:44:23 +0200

    added snack 1 with .map (per Stefano)

--- js/script.js
+++ js/script.js
@@ -3,16 +3,27 @@ const resultEl = document.querySelector('.alert');
 // SNACK 1
 const invitati = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni', 'Fedez', 'George Clooney', 'Amal Clooney', 'Maneskin'];
 
-const segnaposto = [];
-invitati.forEach((element, index) => {
-    const invitato = {
-        guestName: element,
-        tableName: 'Tavolo Vip',
-        place: index + 1
-    }
-    segnaposto.push(invitato);
-});
-console.log(segnaposto);
+let segnaposto = [];
+// invitati.forEach((element, index) => {
+//     const invitato = {
+//         guestName: element,
+//         tableName: 'Tavolo Vip',
+//         place: index + 1
+//     }
+//     segnaposto.push(invitato);
+// });
+// console.log(segnaposto);
+
+// SNACK 1 VARIANTE CON MAP
+const invitatoMap = invitati.map((element, index) => {
+        return    {
+            guestName: element,
+            tableName: 'Tavolo Vip',
+            place: index + 1
+        }
+    });
+console.log(segnaposto);invitatoMap
+
 
 // SNACK 2
 
