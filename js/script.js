6e3a3a8783f74b34d08d3c86d18921340e9773f1
Hycarus <mirkomarasco08@gmail.com>
2023-10-25 15:24:15 +0200

    created snack 4

--- js/script.js
+++ js/script.js
@@ -150,3 +150,52 @@ const res = bikes.filter((element)=>{
     return element.nome && element.peso === Math.min(...min);
 })
 console.log(`La bici che pesa di meno è: ${res[0].nome} e pesa ${res[0].peso}kg`);
+
+
+// SNACK 4
+
+// Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà: Punti fatti e falli subiti.
+// Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadreCalcio = [
+    {nome: "Juve",         
+    punti: 0,         
+    falliSubiti: 0,     
+    },     
+    {         
+    nome: "Milan",         
+    punti: 0,         
+    falliSubiti: 0,     
+    },     
+    {         
+    nome: "Fiorentina",         
+    punti: 0,         
+    falliSubiti: 0,     
+    },     
+    {         
+    nome: "Udinese",         
+    punti: 0,         
+    falliSubiti: 0,     
+    },     
+    {         
+    nome: "Inter",         
+    punti: 0,         
+    falliSubiti: 0,     
+    },
+    {
+    nome: "Napoli",         
+    punti: 0,         
+    falliSubiti: 0,     
+    }
+];
+squadreCalcio.forEach((element, index)=>{
+    const copySquadreCalcio = {...squadreCalcio[index], punti: getRndInteger(1, 100), falliSubiti: getRndInteger(1, 100)}
+    console.log(copySquadreCalcio);
+    const {nome, falliSubiti} = copySquadreCalcio;
+    console.log({nome, falliSubiti});
+});
+
+
+
